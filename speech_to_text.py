c05dff883ee0d682036e4cb2b26b3235b9bda5a7
Huyen Dinh <huyen061610@gmail.com>
2026-02-12 01:27:55 +0200

    init

---
+++ speech_to_text.py
@@ -0,0 +1,17 @@
+# pip3 install speechrecognition 
+# pip3 install pyttsx3 // Text To Speech x3 //pip install pyaudio
+
+import speech_recognition as sr 
+import pyttsx # pip 
+
+def record_text():
+    return 
+
+def output_text(text):
+    return 
+
+while (1):
+    text = record_text() 
+    output_text(text) 
+
+    print("wrote text")
