d54b1a613e7634c0f122f436210d3227e0409034
Huyen Dinh <huyen061610@gmail.com>
2026-02-13 14:43:11 +0200

    add Spectromgram

--- speech_to_text.py
+++ speech_to_text.py
@@ -2,15 +2,37 @@
 # pip3 install pyttsx3 // Text To Speech x3 //pip install pyaudio
 
 import speech_recognition as sr 
-import pyttsx # pip 
+import pyttsx3 # pip 
 
-r = sr.Recognizer() 
+r = sr.Recognizer() # Initialize the recognizer
 
 def record_text():
-    ưhil
+    # Loop in case of errors
+    while(1): 
+        try: 
+            # use the mic as source for input
+            with sr.Microphone() as mic:
+                # prepare recognizer to receive input 
+                r.adjust_for_ambient_noise(mic, duration=0.2) 
+            
+                # listen for the user's input 
+                au = r.listen(mic)
+
+                # using google to recognize audio 
+                txt = r.recognize_google(au)
+
+                return txt 
+
+        except sr.RequestError as e:
+            print(f"Could not request results: {e}")
+        except sr.UnknownValueError: 
+            print("Unknown error occurred")
     return 
 
 def output_text(text):
+    with open("output.txt", "a") as f:
+        f.write(text)
+        f.write("\n")
     return 
 
 while (1):
